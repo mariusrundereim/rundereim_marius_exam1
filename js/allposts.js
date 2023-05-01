9369611550503d082ec4d55fa997098126024bb6
mariusrundereim <marius.rundereim@gmail.com>
2023-05-01 16:49:31 +0200

    show more posts fixed

--- js/allposts.js
+++ js/allposts.js
@@ -2,7 +2,7 @@ import { blogPost } from "./data/exampleposts.js";
 
 
 
-const buttonMore = document.querySelector(".btn-more")
+
 
 
 function displayPosts(blogPost){
@@ -10,7 +10,7 @@ function displayPosts(blogPost){
   const container = document.querySelector(".all-posts")
   container.innerHTML = '';
 
-  for(let i = 0; i < blogPost.length; i++) {
+  for(let i = 0; i < 3; i++) {
     const post = blogPost[i];
     if(post) {
       container.innerHTML += `
@@ -32,7 +32,42 @@ function displayPosts(blogPost){
   </div>
       `
     }
-    
   }
 }
 displayPosts(blogPost)
+
+
+
+// Show more
+
+const buttonMore = document.querySelector(".btn-more")
+
+buttonMore.addEventListener("click", () => {
+  const container = document.querySelector(".all-posts")
+
+  container.innerHTML = "";
+
+  for(let i = 0; i < blogPost.length; i++) {
+    const post = blogPost[i];
+
+    container.innerHTML += `
+      <div class="post-card">
+      <a class="post-card-inner" href="#">
+        <img
+        class="card-image image-prop"
+        src="${post.cover}"
+        alt="image"
+      />
+      <div class="post-details">
+        <p>${post.date}</p>
+        <p>3 min read</p>
+      </div>
+      <h3 class="card-title">
+        ${post.title}
+      </h3>
+    </a>    
+  </div>
+      `
+
+  }
+})
