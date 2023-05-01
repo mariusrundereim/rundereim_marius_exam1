87322bca73ea9b7e95903c4f14ba0c1a98c4476d
mariusrundereim <marius.rundereim@gmail.com>
2023-05-01 14:31:46 +0200

    list of posts page

--- js/lightmode.js
+++ js/lightmode.js
@@ -1,15 +1,19 @@
 let displayTheme = localStorage.getItem("displayTheme");
 const themeBtnToggle = document.querySelector(".toggle-button");
 
-const enableLightMode = () => {
+
+function enableLightMode() {
   document.body.classList.add("light-mode");
   localStorage.setItem("displayTheme", "enabled");
-};
+}
+enableLightMode()
 
-const disableLightMode = () => {
+
+function disableLightMode(){
   document.body.classList.remove("light-mode");
   localStorage.setItem("displayTheme", null);
-};
+}
+disableLightMode()
 
 themeBtnToggle.addEventListener("click", () => {
   displayTheme = localStorage.getItem("displayTheme");
