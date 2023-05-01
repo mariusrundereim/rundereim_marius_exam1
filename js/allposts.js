ed70ed49d33006b31cab3a978732df15e1cb524a
mariusrundereim <marius.rundereim@gmail.com>
2023-05-01 11:54:36 +0200

    errors js

--- js/allposts.js
+++ js/allposts.js
@@ -1,12 +1,49 @@
 import { blogPost } from "./data/exampleposts.js";
 
-const allPosts = document.querySelector(".all-posts")
+
+const buttonMore = document.querySelector(".btn-more")
+
+function displayPosts() {
+    const allPosts = document.querySelector(".all-posts")
+    
+
+    // Clear
+    allPosts.innerHTML = "";
+
+    // Loop 10 first
+    for(let i = 0; i < 10; i++){
+        const posts = allPosts[i]
+
+
+        posts.innerHTML += `
+        <div class="post-card">
+            <a class="post-card-inner" href="#">
+              <img
+              class="card-image image-prop"
+              src="${blogPost[i].cover}"
+              alt="image"
+            />
+            <div class="post-details">
+              <p>${blogPost[i].date}</p>
+              <p>3 min read</p>
+            </div>
+            <h3 class="card-title">
+              ${blogPost[i].title}
+            </h3>
+          </a>    
+        </div>   
+      `;
+    }
+}
+
+displayPosts()
+
+/*
 
 for (let i = 0; i < blogPost.length; i++) {
   //console.log(blogPost[i]);
 
-  allPosts.innerHTML = `
-
+  allPosts.innerHTML += `
   <div class="post-card">
       <a class="post-card-inner" href="#">
         <img
@@ -22,7 +59,8 @@ for (let i = 0; i < blogPost.length; i++) {
         ${blogPost[i].title}
       </h3>
     </a>    
-  </div>
-          
+  </div>   
 `;
 }
+
+*/
