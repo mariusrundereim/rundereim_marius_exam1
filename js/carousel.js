79491b0f29cc10ce9dcf683caf95815700fe57ae
mariusrundereim <marius.rundereim@gmail.com>
2023-05-04 12:18:20 +0200

    post carousel created on index page - missing styling

---
+++ js/carousel.js
@@ -0,0 +1,51 @@
+const url = `https://runder.no/exam1/wp-json/wp/v2/tricks/?per_page=22`;
+
+async function getPosts() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  postCarousel(data);
+  carouselNavigation(data);
+}
+getPosts();
+
+const latestPosts = document.querySelector(".carousel-content");
+let currentSlide = 0;
+
+// Function for carousel navigation
+
+function carouselNavigation(data) {
+  const prevBtn = document.querySelector(".arrow-prev");
+  const nextBtn = document.querySelector(".arrow-next");
+
+  prevBtn.addEventListener("click", () => {
+    currentSlide = Math.max(currentSlide - 3, 0);
+    postCarousel(data);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    currentSlide = Math.min(currentSlide + 3, data.length - 3);
+    postCarousel(data);
+  });
+}
+
+// Function for carousel content
+
+function postCarousel(data) {
+  latestPosts.innerHTML = "";
+  for (let i = currentSlide; i < currentSlide + 3; i++) {
+    const post = data[i];
+
+    const card = document.createElement("div");
+    card.classList.add("post-card");
+    const img = document.createElement("img");
+    img.classList.add("image-prop");
+    img.src = post.acf.src;
+    const text = document.createElement("h3");
+    text.textContent = post.title.rendered;
+    card.appendChild(img);
+    card.appendChild(text);
+    latestPosts.appendChild(card);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
-const url = `https://runder.no/exam1/wp-json/wp/v2/tricks/?per_page=12`;
+const url = `https://runder.no/exam1/wp-json/wp/v2/tricks/?per_page=22`;
 
 async function getPosts() {
   const response = await fetch(url);
   const data = await response.json();
   //console.log(data);
-  getResult(data);
+  postCarousel(data);
 }
 getPosts();
 
-function getResult(data) {
+function postCarousel(data) {
   console.log(data);
-  const latestPosts = document.querySelector(".latest-posts");
+  const latestPosts = document.querySelector(".carousel-content");
+  const prevBtn = document.querySelector(".arrow-prev");
+  const nextBtn = document.querySelector(".arrow-next");
 
+  let currentSlide = 0;
+  renderPosts();
+
+  prevBtn.addEventListener("click", () => {
+    currentSlide = Math.max(currentSlide - 3, 0);
+    renderPosts();
+  });
+
+  nextBtn.addEventListener("click", () => {
+    console.log("click");
+    currentSlide = Math.min(currentSlide + 3, data.length - 3);
+    renderPosts();
+  });
+
+  function renderPosts() {
+    latestPosts.innerHTML = "";
+    for (let i = currentSlide; i < currentSlide + 3; i++) {
+      const post = posts[i];
+      const card = document.createElement("div");
+      card.classList.add("post-card");
+      const img = document.createElement("img");
+      img.src = post.acf.src;
+      const text = document.createElement("h3");
+      text.textContent = post.title.rendered;
+      card.appendChild(img);
+      card.appendChild(text);
+      latestPosts.appendChild(card);
+    }
+  }
+
+  /*
   for (let i = 0; i < data.length; i++) {
     latestPosts.innerHTML = `
     <div class="post-card">
@@ -32,4 +65,5 @@ function getResult(data) {
   </div> 
     `;
   }
+  */
 }
