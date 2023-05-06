77d425b11968ef7744e52bef0493cf168d009749
mariusrundereim <marius.rundereim@gmail.com>
2023-05-06 10:51:53 +0200

    created scroll to top button

--- js/global.js
+++ js/global.js
@@ -1,19 +1,30 @@
-/* Scroll to top */
+function scrollTop() {
+  // Create Element
+  const scrollBtnContainer = document.createElement("div");
+  const scrollButton = document.createElement("button");
+  // Classlist
+  scrollBtnContainer.classList.add("scroll-container");
+  scrollButton.classList.add("scroll-top-icon");
+  // Content
+  scrollButton.innerHTML = `<i class="fas fa-arrow-up"></i>`;
+  // Append Child
+  scrollBtnContainer.appendChild(scrollButton);
+  document.body.appendChild(scrollBtnContainer);
 
-const scrollToTop = document.querySelector(".scroll-top-icon");
-
-scrollToTop.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
+  scrollButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   });
-});
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-    scrollToTop.style.display = "block";
-  } else {
-    scrollToTop.style.display = "none";
-  }
-});
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
+    }
+  });
+}
+scrollTop();
