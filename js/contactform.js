d198666e36ecc7ab7c933de183a5afd6445ee3f6
mariusrundereim <marius.rundereim@gmail.com>
2023-05-10 12:44:28 +0200

    contact form edit

--- js/contactform.js
+++ js/contactform.js
@@ -1,14 +1,18 @@
 const contactForm = document.querySelector(".form-contact");
 const firstName = document.querySelector("#first-name");
+const email = document.querySelector("#email");
+const contactSubject = document.querySelector("#contact-subject");
+const contactMessage = document.querySelector("#contact-message");
+const messageContainer = document.querySelector(".message-container");
 
-console.log(firstName.value);
-
-// firstName.addEventListener("keyup", () => {
-//   console.log(firstName.value);
-// });
+function showMessage(type, message) {
+  const html = `<div class="message ${type}">${message}</div>`;
+  return html;
+}
 
 contactForm.addEventListener("submit", (event) => {
   checkContactForm();
+  showMessage();
   event.preventDefault();
 });
 
@@ -18,5 +22,36 @@ function checkContactForm() {
     console.log("navn finnes");
   } else {
     console.log("Trenger et lengre navn");
+    messageContainer.innerHTML = showMessage(
+      "error",
+      "Name must be more than 5 characters long"
+    );
+  }
+
+  // Email
+  if (email.value) {
+    const regEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+    console.log(regEx.test(email.value));
+  } else {
+    messageContainer.innerHTML = showMessage("error", "Email must be valid");
+  }
+
+  // Subject
+  if (contactSubject.value.length >= 15) {
+    console.log("More than 15 characters long");
+  } else {
+    console.log("Kortere");
+    messageContainer.innerHTML = showMessage("error", "Subject must be longer");
+  }
+
+  // Message
+  if (contactMessage.value.length > 25) {
+    console.log("Mer enn 25 bokstaver langt");
+  } else {
+    console.log("Aaaaaltfor kort");
+    messageContainer.innerHTML = showMessage(
+      "error",
+      "Message should be longer"
+    );
   }
 }
