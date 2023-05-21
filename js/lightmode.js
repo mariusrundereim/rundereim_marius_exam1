95d2cb1d4d9d78bed1b519fe1d7a4637d46d0271
mariusrundereim <marius.rundereim@gmail.com>
2023-05-21 13:24:49 +0200

    logo fix in both display mode

--- js/lightmode.js
+++ js/lightmode.js
@@ -1,12 +1,27 @@
 let displayTheme = localStorage.getItem("displayTheme");
 const themeBtnToggle = document.querySelector(".lightdark-toggle");
+let pageLogo = document.querySelector("#page-logo")
 
 function enableLightMode() {
+  pageLogo.innerHTML = `
+  <img
+    class="site-logo"
+    src="/images/svg/sv_logo_dark.svg"
+    alt="Page Logo"
+  />
+  `
   document.body.classList.add("light-mode");
   localStorage.setItem("displayTheme", "enabled");
 }
 
 function disableLightMode() {
+  pageLogo.innerHTML = `
+  <img
+      class="site-logo"
+      src="/images/svg/sv_logo_white.svg"
+      alt="Page Logo"
+    />
+  `
   document.body.classList.remove("light-mode");
   localStorage.setItem("displayTheme", "disabled");
 }
